10ea85d57f158a9931b9cad9d3e45ad28e63ef9e
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-03-09 20:12:29 +0000

    Initial Commit

---
+++ src/components/Home/Hero/Hero.js
@@ -0,0 +1,15 @@
+import styles from './Hero.modules.css'
+
+import React from 'react'
+
+function Hero() {
+  return (
+    <section className={styles.hero}>
+        <div className={styles.card}>
+            <img src={''} alt=''></img>
+        </div>
+    </section >
+  )
+}
+
+export default Hero
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import styles from './Home.module.css'
+
+import React from 'react'
+
+function Home() {
+  return (
+    <section className={styles.home}>
+        
+    </section>
+  )
+}
+
+export default Home
