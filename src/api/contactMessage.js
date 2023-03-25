826950a5890c18faae654e7d62c36cc2ff1ccb93
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-03-25 12:02:02 +0000

    added Conctacts messaging functionality

---
+++ src/api/contactMessage.js
@@ -0,0 +1,29 @@
+import baseURL from './baseURL' 
+
+const createContactMessage  = async (data) => {
+    const url = baseURL + '/core/create-message/'
+    try {
+        let response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json",
+
+            },
+            body: JSON.stringify(data)
+            })
+        let created = await response.json()        
+        if (response.ok) {
+            return created
+        }
+        else {
+            throw created
+        }
+    } catch {
+        console.log('this is an error in the api service')
+    }
+}
+
+export {
+    createContactMessage,
+    
+}
--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,13 +1,33 @@
 import styles from './Contacts.module.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { createContactMessage } from '../../api/contactMessage'
 
 function Contacts() {
 
     const [formData, setFormData] = useState({
-                                            email: null,
-                                            name: null,
-                                            message: null,                                            
+                                            email: undefined,
+                                            title: undefined,
+                                            message: undefined,                                            
     })
+
+
+    const onValueChange = (e) => {
+        setFormData((state) => ({...state, [e.target.name]: e.target.value}))
+
+    }
+
+    const onSend  = (e) => {
+        e.preventDefault()
+        console.log('formData in onsend', formData)
+        createContactMessage(formData)
+            .then((res) => {
+                
+                console.log('res in onsend', res);
+            })
+            .catch((res) => {
+                console.log('res in ERRORonsend', res);
+            })    
+    }
 
     return (
         <section className={styles.contacts}>
@@ -19,20 +39,36 @@ function Contacts() {
 
             <div className={styles.form_box}>
                 <h1>CONTACTS</h1>
-                    <form className={`${styles.form}`}>
+                    <form onSubmit={onSend} className={`${styles.form}`}>
                         <div className={`${styles.field_box} ${styles.field_1}`}>
-                            <label>Email</label>
-                            <input id={'email'} type={'email'} ></input>
+                            <label htmlFor='email'>Email</label>
+                            <input
+                                name='email' 
+                                value={formData.email}
+                                onChange={onValueChange}
+                                id={'email'}
+                                type={'email'} 
+                                placeholder={'enter your email'}></input>
                         </div>
 
                         <div className={`${styles.field_box} ${styles.field_2}`}>
-                            <label>Name</label>
-                            <input id={'name'} type={'text'}></input>
+                            <label htmlFor='title'>Title</label>
+                            <input 
+                                name='title' 
+                                value={formData.title}
+                                onChange={onValueChange}
+                                id={'title'} type={'text'}
+                                placeholder={'enter title of message'}></input>
                         </div>
 
                         <div className={`${styles.field_box} ${styles.field_3}`}>
-                            <label></label>
-                            <textarea id={'message'}></textarea>
+                            <textarea      
+                                name='message' 
+                                value={formData.message}
+                                onChange={onValueChange}
+                                id={'message'} 
+                                placeholder={'Enter Your Message Here'}
+                                ></textarea>
                         </div>
 
                         <button className={`${styles.field_box} ${styles.field_4}`}>Send</button>
