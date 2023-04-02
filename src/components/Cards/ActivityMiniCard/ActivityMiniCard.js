23ed6fde88214ed836fb15a74ab2f86264c5e1e8
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-04-02 23:05:03 +0100

    modidfied Dashboard

---
+++ src/components/Cards/ActivityMiniCard/ActivityMiniCard.js
@@ -0,0 +1,78 @@
+import styles from './ActivityMiniCard.module.css'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { getActivity } from '../../../api/activities'
+
+
+
+function ActivityMiniCard({activity, onDelete}) {
+    const [activityExercices, setActivityExercises] = useState([])
+    const handleDragStart = (e) => e.preventDefault();
+    useEffect(() => {
+        getActivity(activity.id)
+            .then((res) => {
+                setActivityExercises(res.exercises)
+            })
+            .catch((res) => { })
+    }, [])
+
+
+  return (
+    <div onDragStart={handleDragStart} role="presentation"  className={styles.card}>
+        <div className={styles.card_info}>
+                <Link className={`${styles.card_cell_1} ${'title_outlined'}`} to={`/activity/${activity.id}`}>
+                    <p >
+                    {activity.name}</p>
+                </Link>
+            {/* <span className={`${styles.card_cell_2} ${styles.card_cell}`}>
+                Info: {activity.description}</span> */}
+            <span className={`${styles.card_cell_3} ${styles.card_cell}`}>
+                Duration: {activity.duration} min</span>
+                {/* TODO FIX TYPE VISUALIZATION */}
+            <span className={`${styles.card_cell_4} ${styles.card_cell}`}>
+                Type: {activity.type}</span>
+            {activity.type === 3 ?
+                <>
+                    <span className={`${styles.card_cell_5} ${styles.card_cell}`}>
+                        Distance: {activity.sets}km</span>
+                        {/* TODO pace field in right format 6:10km/h */}
+                    <span className={`${styles.card_cell_6} ${styles.card_cell}`}>
+                        Pace: {activity.reps}min/km</span>
+                    <span className={`${styles.card_cell_7} ${styles.card_cell}`}>
+                        Speed: {activity.weights}km/h</span>
+                    <span className={`${styles.card_cell_8} ${styles.card_cell}`}>
+                        HR: {activity.cues}bpm</span>
+                    <span className={`${styles.card_cell_9} ${styles.card_cell}`}>
+                        RPE: {activity.rpe}</span>
+                </>
+                : null    
+                }
+                {/* TODO Button: "Add Exercise" in activity with any type*/}
+            <span className={`${styles.card_cell_10} ${styles.card_cell}`}>
+                Exercises: {activityExercices.length !== 0 ?
+                 activityExercices.map((ex) => 
+                                <span className={`${styles.exercise} ${''}`}
+                                    key={ex.id}
+                                    >
+                                    {ex.name}
+
+                                </span>)
+                            : null
+                }
+            </span>
+            <Link className={`${styles.card_cell_11} ${styles.card_cell} ${styles.card_cell_link}`} 
+                to={`/edit-activity/${activity.id}/`}>
+                Edit
+            </Link>
+                {/* TODO  add confirmation modal */}
+            <Link className={`${styles.card_cell_12} ${styles.card_cell} ${styles.card_cell_link}`}  
+                onClick={() => onDelete(activity.id)}>
+                Delete
+            </Link>
+
+        </div>
+    </div>
+  )
+}
+
+export default ActivityMiniCard
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect } from 'react'
+import "react-alice-carousel/lib/alice-carousel.css";
+import React, { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { getAllActivities } from '../../api/activities'
 import { getAllExercises } from '../../api/exercises'
@@ -6,34 +7,48 @@ import { getProfile } from '../../api/profile'
 import { UserContext } from '../../contexts/UserContext'
 import CardReel from '../Cards/CardReel/CardReel'
 import styles from './Dashboard.module.css'
+import 'react-alice-carousel/lib/alice-carousel.css';
+import AliceCarousel from "react-alice-carousel";
+import ActivityMiniCard from "../Cards/ActivityMiniCard/ActivityMiniCard";
+import ActivityCard from "../Activities/AllActivities/ActivityCard/ActivityCard";
+import ExerciseCard from "../Exercises/AllExercises/ExerciseCard/ExerciseCard";
+import ListCard from "../Cards/ListCard/ListCard";
+
+const handleDragStart = (e) => e.preventDefault();
+
+const items = [
+  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
+  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
+  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
+];
 
 
 function Dashboard() {
-
+    const [activities, setActivities] = useState([])
+    const [exercises, setExercises] = useState([])
+    const [profile, setProfile] = useState([])
     const { user, setUser } = useContext(UserContext) 
-
-
+    console.log('profile in dash' , profile)
+    const activitiesCarded = activities.map((activity) => <ActivityMiniCard activity={activity}/>)
+    const exercisesCarded = exercises.map((exercise) => <ExerciseCard  exercise={exercise}/>)
     useEffect(() => {
         getAllExercises(user)
             .then((res) => {
-                console.log('dash all exers', res);
+                setExercises(res)
             })
             .catch((res) => {
-                console.log('dash', res);
             })
         getAllActivities(user)
             .then((res) => {
-                console.log('dash all acts', res);
+                setActivities(res)
             })
             .catch((res) => {
-                console.log('dash', res);
             })
         getProfile(user)
             .then((res) => {
-                console.log('dash profile', res);
+                setProfile(res)
             })
             .catch((res) => {
-                console.log('dash', res);
             })
     }, [])
 
@@ -43,15 +58,20 @@ function Dashboard() {
     <section className={styles.dashboard}>
         <div className={styles.grid}>
             <div className={`${styles.grid_card} ${styles.grid_card_1}`}>
-                <img src='/images/defaults/default-user.png' alt='' />
+                {profile.image_local ?
+                 <img src={`${profile.image_local}`} alt='' /> :
+                  <img src='/images/defaults/default-user.png' alt='' />}
             </div>
             <div className={`${styles.grid_card} ${styles.grid_card_2}`}>
-                <h3>LINKS</h3>
+                <p>Quick Links</p>
                 <ul>
-                    <li><Link to='/all-exercises'>All Exercises</Link></li>
-                    <li><Link to='/all-activities'>All Activities</Link></li>
-                    <li><Link to='/'></Link></li>
-                    <li><Link to='/'></Link></li>
+                    <li><Link to={'/'}>Profile</Link></li>
+                    <li><Link to={'/'}>Activities</Link></li>
+                    <li><Link to={'/'}>Exercises</Link></li>
+                    <li><Link to={'/'}>Periodization</Link></li>
+                    <li><Link to={'/'}>Cycles</Link></li>
+                    <li><Link to={'/'}>Goals</Link></li>
+                    <li><Link to={'/'}>Defenders</Link></li>
                 </ul>
             </div>
             <div className={`${styles.grid_card} ${styles.grid_card_3}`}>
@@ -63,20 +83,23 @@ function Dashboard() {
                 <img src='/images/backgrounds/background-01.jpg' alt='' />
             </div>
             <div className={`${styles.grid_card} ${styles.grid_card_5}`}>
-                <h3>Jack Username</h3>
+                <h3>{profile.first_name} {profile.last_name}</h3>
                 <ul>
-                    <li>Age: 36</li>
-                    <li>Location: London, UK</li>
-                    <li>Prime Sport: Resistance Training</li>
+                    <li>DOB: {profile.dob}</li>
+                    <li>Phone: {profile.phone}</li>
+                    <li>Email: {user.email}</li>
+                    <li>Gender: {profile.gender}</li>
+                    <li>Prime Sport: All In One</li>
                 </ul>
             </div>
             <div className={`${styles.grid_card} ${styles.grid_card_6}`}>
                 <h3>Activities</h3>
-                    <CardReel />
+                <AliceCarousel mouseTracking items={activitiesCarded} />
             </div>
             <div className={`${styles.grid_card} ${styles.grid_card_7}`}>
                 <h3>Exercises</h3>
-                    <CardReel />
+                <AliceCarousel mouseTracking items={exercisesCarded} />
+
             </div>
             <div className={`${styles.grid_card} ${styles.grid_card_8}`}>
                 <h3>Exercises STATS</h3>
