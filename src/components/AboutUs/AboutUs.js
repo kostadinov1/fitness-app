1a7ec7d5716f1baab49977f2b3d9004e974a9766
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-04-08 10:26:05 +0100

    modified home banner

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -15,27 +15,26 @@ function AboutUs() {
         </div>
         <div className={`${styles.description}`}>
             <p>
-                This website is create in 2023 to help user achieve their goals.
+                This website is created in 2023 to help user achieve their goals.
                 Whith custom built exercises, activities and periodization cycles
-                our website gives You the ability to built and organize Your training
-                programs with ease.
+                our website gives You the ability to built and organize Your own training
+                protocols with ease.
             </p>
             <p>
                 This web application give You the ability to create Your custom
                 Exercises which You can group in Activities, which can be added to Your 
                 Calendar. All Activities can be organized periodicaly in cycles which can
                 have Goals and Defenders.
-                This sounds quite complicated initially, but for people who need this
-                certain features, this app can be really useful.
+                If You have the knowledge how to organize your workouts in an optimal way, 
+                You can build almost any program your mind can think of!
                 Once You built and organize your workouts(activites), You can enjoy just
-                tweaking and fine tuning Your programs to fit Your needs.
+                tweaking and fine tuning Your programs to fit Your needs for the future needs.
             </p>
-
             <p>
                 In the near future we consider adding functionality for tracking 
                 different aspects of Your trainng process and You will be able 
-                to enjoy full functionality. For now You can start
-                enjoing our services clicking on the link below.
+                to enjoy even more features. For now You can start
+                using our services by clicking on the link below.
             </p>
         </div> 
         <Link className={`${styles.title_link}`} to={'/register'}>
--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -5,10 +5,31 @@ import styles from './Banner.module.css'
 const Banner = () => {
   return (
     <div className={`${styles.banner}`}>
-        <h1>Hello and Welcome to Our Website</h1>
-        <h3>Here you can Create and Periodize Your WorkOuts for Free!</h3>
-        <h4>What is Periodization?</h4>
-        <p>"The manipulation of training variable to enhance progress, reduce injury risk , and time peak performances."</p>
+        <div className={`${styles.title}`}>Hello and Welcome to Our Website</div>
+        <div className={`${styles.subtitle}`}>Here you can Create and Organize Your Activities for Free!</div>
+        <div className={`${styles.pdiv}`}>
+          <hr/>
+          What can you find here?
+          <hr/>
+          <p>
+                This website is created in 2023 to help user achieve their goals.
+                Whith custom built exercises, activities and periodization cycles
+                our website gives You the ability to built and organize Your own training
+                protocols with ease.
+            </p>
+            <hr/>
+            <p>
+                This web application give You the ability to create Your custom
+                Exercises which You can group in Activities, which can be added to Your 
+                Calendar. All Activities can be organized periodicaly in cycles which can
+                have Goals and Defenders.
+                If You have the knowledge how to organize your workouts in an optimal way, 
+                You can build almost any program your mind can think of!
+                Once You built and organize your workouts(activites), You can enjoy just
+                tweaking and fine tuning Your programs to fit Your needs for the future needs.
+            </p>
+        {/* <p>"The manipulation of training variable to enhance progress, reduce injury risk , and time peak performances."</p> */}
+        </div>
     </div>
   )
 }
